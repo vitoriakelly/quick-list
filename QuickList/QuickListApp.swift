695330075ca9fdd5a2d148989274f4eeba24f804
Vitoria Kelly <vitoriacostal@alu.ufc.br>
2026-04-10 15:47:16 -0300

    Initial Commit

---
+++ QuickList/QuickListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickListApp.swift
+//  QuickList
+//
+//  Created by Vitória Kelly on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
